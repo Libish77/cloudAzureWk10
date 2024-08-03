2a07c53449ffe5d74b37e0b496e65e1c2936d355
Libish prajapati <35631749+Libish77@users.noreply.github.com>
2024-08-03 13:08:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.set('views', path.join(__dirname, 'views'));
 // Database configuration
 const config = {
     user: 'azureuser',
-    password: 'P@ssw0rdssss',
-    server: 'wk10556.database.windows.net',
-    database: 'wk10',
+    password: 'P@ssw0rd',
+    server: 'wk13t3.database.windows.net',
+    database: 'week13',
   options: {
     encrypt: true,
     enableArithAbort: true
